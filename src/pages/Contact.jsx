9d2504985f9b72584e88faf007d967e8758b7b94
Твоё Имя <itdan.rb@gmail.com>
2026-06-13 23:25:58 +0300

    Fix ESLint errors in Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -48,7 +48,7 @@ function Contact() {
       <p className="contact-alt">
         Или напишите напрямую: 
         <a href="https://t.me/твой_ник" target="_blank" rel="noopener">Telegram</a> | 
-        <a href="tel:+79991234567">+7 (999) 123-45-67</a>
+        <a href="tel:+79218909501">+7 (921) 890-95-01</a>
       </p>
     </div>
   );
