827d210b52f94d8c2342dea06b67cf54b6429e09
Твоё Имя <itdan.rb@gmail.com>
2026-07-08 17:35:12 +0300

    styling5

--- src/data/services.js
+++ src/data/services.js
@@ -8,12 +8,20 @@ export const servicesList = [
     image: "https://www.ecoera.ru/wp-content/uploads/2021/12/gidrodinamicheskaya-prochistka-truby.png"
   },
   {
-    id: 1,
+    id: 2,
     title: "Механическая прочистка",
     price: "от 1000 рублей",
     priceValue: 1000,
     desc: "Метод очистки, разрушает мусор с помощью длинного гибкого троса, попадая в самые труднодоступные места.",
     image: "https://www.vkservice71.ru/wp-content/uploads/2017/05/008.jpg"
   },
-  
+  {
+    id: 3,
+    title: "Химическая прочистка",
+    price: "от 1000 рублей",
+    priceValue: 1000,
+    desc: "Метод очистки, рахрушиющий загрязнения щелочами и кислотами.",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3YOBZZ1DQGlXiN7jQylck8-ayiGBKAmAYA2TI2M_URcm5j_i7egoquwxO&s=10"
+  },
+
 ];
