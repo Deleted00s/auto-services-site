872a5d796b37edea6e9df01d30458aa71f532221
Твоё Имя <itdan.rb@gmail.com>
2026-07-14 19:32:28 +0300

    expo2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -81,7 +81,7 @@ function Home() {
           </div>
           <div className="about-image">
             <img 
-              src="/images/about-team.png" 
+              src="/images/1397.jpg" 
               alt="Команда Засор Канал Профи" 
               loading="lazy"
             />
